014510a92656580492b69d92ddaf1c791bd2d4c1
Dilum Sanjaya <dilumsanjaya@gmail.com>
2019-02-11 00:10:29 +0530

    Updates

---
+++ precache-manifest.7d9978d7765f853bd56e159250a53756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "706715cccb0f88cd90f8",
+    "url": "/olivegrove/static/js/runtime~main.706715cc.js"
+  },
+  {
+    "revision": "421e69257d5d44427818",
+    "url": "/olivegrove/static/js/main.421e6925.chunk.js"
+  },
+  {
+    "revision": "e078c9d55bc4f59b851e",
+    "url": "/olivegrove/static/js/1.e078c9d5.chunk.js"
+  },
+  {
+    "revision": "421e69257d5d44427818",
+    "url": "/olivegrove/static/css/main.bfe05b7d.chunk.css"
+  },
+  {
+    "revision": "cbbf45c008ea5c1db7522415c34ee39e",
+    "url": "/olivegrove/index.html"
+  }
+];
